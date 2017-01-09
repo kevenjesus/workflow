0cf86bd14f0f348debfbd92f7e8deaa6dae6002c
claudio <claudio@negociosreais.com.br>
2017-01-08 22:17:56 -0200

    finalizando projeto

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minifyHtml = require('gulp-minify-html');
+var minifyScss = require('gulp-minify-css');
+
+
+gulp.task('compila-scss', function() {
+	return gulp.src('source/scss/**/*.scss')
+	.pipe(sass())
+	.pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('minify-scss', function() {
+	return gulp.src('source/scss/**/*.scss')
+		.pipe(minifyScss())
+		.pipe(gulp.dest('./dist/css'));
+})
+
+gulp.task('compila-html', function() {
+	return gulp.src('source/index.html')		
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify-html', function() {
+	return gulp.src('source/index.html')
+		.pipe(minifyHtml())
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('./source/scss/*.scss',['compila-scss']);
+	gulp.watch('./source/index.html',['compila-html']);
+});
+
+gulp.task('default',['compila-scss','minify-scss','compila-html','minify-html','watch']);
