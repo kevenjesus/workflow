c8cb7bfd9fad123fba73d74e91c059e2bdbddddc
claudio <claudio@negociosreais.com.br>
2017-01-10 14:04:53 -0200

    Finalizando atividade1

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var minifyHtml = require('gulp-minify-html');
-var minifyScss = require('gulp-minify-css');
+// var minifyHtml = require('gulp-minify-html');
+// var minifyScss = require('gulp-minify-css');
 
 gulp.task('compila-scss', function() {
 	return gulp.src('source/scss/**/*.scss')
@@ -9,26 +9,26 @@ gulp.task('compila-scss', function() {
 	.pipe(gulp.dest('./dist/css'))
 });
 
-gulp.task('minify-scss', function() {
-	return gulp.src('source/scss/**/*.scss')
-		.pipe(minifyScss())
-		.pipe(gulp.dest('./dist/css'));
-})
+// gulp.task('minify-scss', function() {
+// 	return gulp.src('source/scss/**/*.scss')
+// 		.pipe(minifyScss())
+// 		.pipe(gulp.dest('./dist/css'));
+// })
 
 gulp.task('compila-html', function() {
 	return gulp.src('source/index.html')		
 	.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('minify-html', function() {
-	return gulp.src('source/index.html')
-		.pipe(minifyHtml())
-		.pipe(gulp.dest('./dist'));
-});
+// gulp.task('minify-html', function() {
+// 	return gulp.src('source/index.html')
+// 		.pipe(minifyHtml())
+// 		.pipe(gulp.dest('./dist'));
+// });
 
 gulp.task('watcho', function() {
 	gulp.watch('./source/scss/*.scss',['compila-scss']);
 	gulp.watch('./source/index.html',['compila-html']);
 });
 
-gulp.task('default',['compila-scss','minify-scss','compila-html','minify-html','watcho']);
+gulp.task('default',['compila-scss','compila-html','watcho']);
